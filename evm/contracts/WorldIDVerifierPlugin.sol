2cf79cae357cb6ed3d5a2d9391d61ca52fa14e7a
Paul Burke <paul@paulburke.co>
2023-08-07 13:41:01 -0400

    Switches to use PluginCloneable proxy contract

--- evm/contracts/WorldIDVerifierPlugin.sol
+++ evm/contracts/WorldIDVerifierPlugin.sol
@@ -2,9 +2,18 @@
 pragma solidity ^0.8.0;
 
 import "hardhat/console.sol";
-import {Plugin, IDAO} from '@aragon/osx/core/plugin/Plugin.sol';
+import {PluginCloneable, IDAO} from '@aragon/osx/core/plugin/PluginCloneable.sol';
 
-contract WorldIDVerifierPlugin is Plugin {
+contract WorldIDVerifierPlugin is PluginCloneable {
 
+    /// @param _dao The associated DAO.
     constructor(IDAO _dao) Plugin(_dao) {}
+
+    /// @notice Initializes the contract.
+    /// @param _dao The associated DAO.
+    /// @param _admin The address of the admin.
+    function initialize(IDAO _dao, address _admin) external initializer {
+        __PluginCloneable_init(_dao); // Must call to be associated with a DAO and use the DAO's PermissionManager
+        admin = _admin;
+    }
 }
